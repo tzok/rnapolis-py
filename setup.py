02ea75d3c2714b448044804fe085165bbca213d9
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2022-08-30 16:13:49 +0200

    feat: release 0.0.4 (#3)
    * chore: set version to 1.0.0.dev1
    
    * feat: add the first test
    
    * feat: add new output options (#2)
    
    * feat: add BPSEQ output
    
    * feat: add options to generate CSV and BPSEQ files
    
    * feat: add support for extended dot-bracket notation
    
    * chore: set version to 1.0.0.dev1
    
    * feat: add the first test
    
    * feat: add new output options (#2)
    
    * feat: add BPSEQ output
    
    * feat: add options to generate CSV and BPSEQ files
    
    * feat: add support for extended dot-bracket notation
    
    * chore: set version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.0.3",
+    version="0.0.4",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
