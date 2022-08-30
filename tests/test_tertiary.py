e7d76ebc5d0bd09e5a2b4cddd23faa1f6420d0dc
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2022-08-30 15:16:04 +0200

    feat: add the first test

---
+++ tests/test_tertiary.py
@@ -0,0 +1,13 @@
+import math
+
+from rnapolis.tertiary import Atom, torsion_angle
+
+
+def test_torsion_angle():
+    a1 = Atom(None, None, 1, "P", 50.63, 49.73, 50.57)
+    a2 = Atom(None, None, 1, "O5'", 50.16, 49.14, 52.02)
+    a3 = Atom(None, None, 1, "C5'", 50.22, 49.95, 53.21)
+    a4 = Atom(None, None, 1, "C4'", 50.97, 49.23, 54.31)
+    assert math.isclose(
+        math.degrees(torsion_angle(a1, a2, a3, a4)), -127.83976634524326
+    )
