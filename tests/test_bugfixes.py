3eaf92b90d2b1ad20f1a93af183af55b6d898e9a
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2022-09-08 15:20:10 +0200

    feat: add a test case where two bases are oriented in 45 degrees

--- tests/test_bugfixes.py
+++ tests/test_bugfixes.py
@@ -4,6 +4,7 @@ from rnapolis.parser import read_3d_structure
 from rnapolis.tertiary import Mapping2D3D
 
 
+# in 1E7K there is a break in a chain, so it should be recognized as separate strands
 def test_1E7K():
     with open("tests/1E7K_1_C.cif") as f:
         structure3d = read_3d_structure(f, 1)
@@ -12,6 +13,7 @@ def test_1E7K():
     assert len(mapping.strands_sequences) == 2
 
 
+# in 1DFU the adjacent U and G seem to be base-pair like if you do not take into account angles
 def test_1DFU():
     with open("tests/1DFU_1_M-N.cif") as f:
         structure3d = read_3d_structure(f, 1)
@@ -28,9 +30,19 @@ def test_1DFU():
     assert b1u not in mapping.base_pair_graph[b2g]
 
 
+# in 4WTI the first residue has only O3' atom and so is not considered a nucleotide
 def test_4WTI():
     with open("tests/4WTI_1_T-P.cif") as f:
         structure3d = read_3d_structure(f, 1)
     structure2d = extract_secondary_structure(structure3d)
     mapping = Mapping2D3D(structure3d, structure2d)
     assert mapping.dot_bracket == ">strand_T\nCGG\n.((\n>strand_P\nCC\n))"
+
+
+# in 1HMH the bases are oriented in 45 degrees and it caused the program to identify invalid base pair
+def test_1HMH():
+    with open("tests/1HMH_1_E.cif") as f:
+        structure3d = read_3d_structure(f, 1)
+    structure2d = extract_secondary_structure(structure3d)
+    mapping = Mapping2D3D(structure3d, structure2d)
+    assert mapping.dot_bracket == ">strand_E\nUG\n.."
