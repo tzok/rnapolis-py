244b674e7bf8e6b00fe009903ea826d46f4eeef0
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2022-12-21 17:29:27 +0100

    feat: adapt the tests to API changes

--- tests/test_metareader.py
+++ tests/test_metareader.py
@@ -1,8 +1,9 @@
 from rnapolis.metareader import list_metadata, read_metadata
+from rnapolis.util import handle_input_file
 
 
 def test_list_metadata():
-    assert ["atom_site"] == list_metadata("tests/1JJP.cif")
+    assert ["atom_site"] == list_metadata(handle_input_file("tests/1JJP.cif"))
     assert [
         "audit_author",
         "struct",
@@ -24,11 +25,11 @@ def test_list_metadata():
         "pdbx_entity_nonpoly",
         "pdbx_entity_remapping",
         "atom_site",
-    ] == list_metadata("tests/1ehz-assembly-1.cif")
+    ] == list_metadata(handle_input_file("tests/1ehz-assembly-1.cif"))
 
 
 def test_read_metadata():
-    result = read_metadata("tests/1ehz-assembly-1.cif", ["struct"])
+    result = read_metadata(handle_input_file("tests/1ehz-assembly-1.cif"), ["struct"])
     assert "struct" in result
     assert len(result["struct"]) > 0
     assert "title" in result["struct"][0]
