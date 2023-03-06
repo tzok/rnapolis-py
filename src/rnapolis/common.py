1701386b5d784cf305e006eeb366872c7f551a36
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2023-03-06 07:44:07 +0100

    feat: add BPSEQ, DotBracket and motif classes

--- src/rnapolis/common.py
+++ src/rnapolis/common.py
@@ -1,4 +1,6 @@
+import logging
 import string
+from collections.abc import Sequence
 from dataclasses import dataclass
 from enum import Enum
 from functools import cache, total_ordering
@@ -318,3 +320,299 @@ class Structure2D:
     baseRiboseInteractions: List[BaseRibose]
     basePhosphateInteractions: List[BasePhosphate]
     otherInteractions: List[OtherInteraction]
+
+
+@dataclass
+class Entry(Sequence):
+    index_: int
+    sequence: str
+    pair: int
+
+    def __getitem__(self, item):
+        if item == 0:
+            return self.index_
+        elif item == 1:
+            return self.sequence
+        elif item == 2:
+            return self.pair
+        raise IndexError()
+
+    def __len__(self) -> int:
+        return 3
+
+    def __str__(self):
+        return f"{self.index_} {self.sequence} {self.pair}"
+
+
+@dataclass
+class Strand:
+    first: int
+    last: int
+    sequence: str
+
+    @staticmethod
+    def from_bpseq_entries(entries: List[Entry], reverse: bool = False):
+        first = entries[0].index_
+        last = first + len(entries) - 1
+        if reverse:
+            first, last = last, first
+        sequence = "".join(
+            [
+                entry.sequence
+                for entry in (entries if not reverse else reversed(entries))
+            ]
+        )
+        return Strand(first, last, sequence)
+
+    def __str__(self):
+        return f"{self.first}-{self.sequence}-{self.last}"
+
+
+@dataclass
+class Stem:
+    strand5p: Strand
+    strand3p: Strand
+
+    @staticmethod
+    def from_bpseq_entries(strand5p_entries: List[Entry], all_entries: List):
+        paired = set([entry[2] for entry in strand5p_entries])
+        strand3p_entries = list(filter(lambda entry: entry[0] in paired, all_entries))
+        return Stem(
+            Strand.from_bpseq_entries(strand5p_entries),
+            Strand.from_bpseq_entries(strand3p_entries, reverse=True),
+        )
+
+    def __str__(self):
+        return f"{self.strand5p} {self.strand3p}"
+
+
+@dataclass
+class Hairpin:
+    strand: Strand
+
+    def __str__(self):
+        return f"{self.strand}"
+
+
+@dataclass
+class SingleStrand:
+    strand: Strand
+
+    def __init__(self, strand: Strand):
+        self.strand = strand
+
+    def __str__(self):
+        return f"{self.strand}"
+
+
+@dataclass
+class BpSeq:
+    entries: List[Entry]
+
+    @staticmethod
+    def from_file(bpseq_path: str):
+        entries = []
+        with open(bpseq_path) as fd:
+            for line in fd:
+                fields = line.strip().split()
+                if len(fields) != 3:
+                    logging.warning("Failed to find 3 columns in BpSeq line: {}", line)
+                    continue
+                entry = Entry(int(fields[0]), fields[1], int(fields[2]))
+                entries.append(entry)
+        return BpSeq(entries)
+
+    def __post_init__(self):
+        self.pairs = {}
+        for i, _, j in self.entries:
+            if j != 0:
+                self.pairs[i] = j
+                self.pairs[j] = i
+
+    def __str__(self):
+        return "\n".join(("{} {} {}".format(i, c, j) for i, c, j in self.entries))
+
+    def __eq__(self, other):
+        return len(self.entries) == len(other.entries) and all(
+            ei == ej for ei, ej in zip(self.entries, other.entries)
+        )
+
+    def sequence(self) -> str:
+        return "".join(entry.sequence for entry in self.entries)
+
+    def paired(self, only5to3: bool = False):
+        result = filter(lambda entry: entry.pair != 0, self.entries)
+        if only5to3:
+            result = filter(lambda entry: entry.index_ < entry.pair, result)
+        return result
+
+    def stems(self) -> List:
+        result = []
+        strand5p: List[Entry] = []
+
+        for entry in self.paired(only5to3=True):
+            if not strand5p:
+                strand5p.append(entry)
+                continue
+
+            i, _, j = entry
+            k, _, l = strand5p[-1]
+            if i == k + 1 and j == l - 1:
+                strand5p.append(entry)
+                continue
+
+            result.append(Stem.from_bpseq_entries(strand5p, self.entries))
+            strand5p = [entry]
+
+        if strand5p:
+            result.append(Stem.from_bpseq_entries(strand5p, self.entries))
+
+        return result
+
+    def hairpins(self) -> List:
+        hairpins = []
+
+        for entry in self.paired(only5to3=True):
+            if all([self.pairs[i] == 0 for i in range(entry.index_ + 1, entry.pair)]):
+                entries = list(
+                    filter(
+                        lambda e: entry.index_ <= e.index_ <= entry.pair, self.entries
+                    )
+                )
+                strand = Strand.from_bpseq_entries(entries)
+                hairpins.append(Hairpin(strand))
+
+        return hairpins
+
+    def single_strands(self):
+        stops = set()
+        n = len(self.entries)
+
+        for i in range(n):
+            if i > 0 and self.entries[i].pair == 0 and self.entries[i - 1].pair != 0:
+                stops.add(i - 1)
+            if (
+                i < n - 1
+                and self.entries[i].pair == 0
+                and self.entries[i + 1].pair != 0
+            ):
+                stops.add(i + 1)
+
+        if not stops:
+            return []
+
+        stops = sorted(stops)
+        single_strands = []
+
+        for i in range(1, len(stops)):
+            j, k = stops[i - 1], stops[i]
+            entry_j, entry_k = self.entries[j], self.entries[k]
+
+            if entry_j.pair != entry_k.index and entry_j.index != entry_k.index - 1:
+                entries = list(
+                    filter(
+                        lambda e: entry_j.index <= e.index <= entry_k.index,
+                        self.entries,
+                    )
+                )
+                if all([entry.pair == 0 for entry in entries[1:-1]]):
+                    strand = Strand.from_bpseq_entries(entries)
+                    single_strands.append(SingleStrand(strand))
+
+        # 5'
+        if stops[0] != 0:
+            entry = self.entries[stops[0]]
+            entries = list(filter(lambda e: e.index <= entry.index, self.entries))
+            strand = Strand.from_bpseq_entries(entries)
+            single_strands.insert(0, SingleStrand(strand))
+
+        # 3'
+        if stops[-1] != len(self.entries) - 1:
+            entry = self.entries[stops[-1]]
+            entries = list(filter(lambda e: entry.index <= e.index, self.entries))
+            strand = Strand.from_bpseq_entries(entries)
+            single_strands.append(SingleStrand(strand))
+
+        return single_strands
+
+    def fcfs(self):
+        stems = [
+            (stem.strand5p.first, stem.strand3p.first, len(stem.strand5p.sequence))
+            for stem in self.stems()
+        ]
+        orders = [0 for i in range(len(stems))]
+
+        for i in range(1, len(stems)):
+            k, l, _ = stems[i]
+            available = [True for i in range(10)]
+
+            for j in range(i):
+                m, n, _ = stems[j]
+                conflicted = (k < m < l < n) or (m < k < n < l)
+
+                if conflicted:
+                    available[orders[j]] = False
+
+            order = next(filter(lambda i: available[i] is True, range(len(available))))
+            orders[i] = order
+
+        sequence = self.sequence()
+        structure = ["." for i in range(len(sequence))]
+        brackets = ["()", "[]", "{}", "<>", "Aa", "Bb", "Cc"]
+
+        for i, stem in enumerate(stems):
+            bracket = brackets[orders[i]]
+            j, k, n = stem
+
+            while n > 0:
+                structure[j - 1] = bracket[0]
+                structure[k - 1] = bracket[1]
+                j += 1
+                k -= 1
+                n -= 1
+
+        structure = "".join(structure)
+
+        return DotBracket.from_string(sequence, structure)
+
+
+@dataclass
+class DotBracket:
+    sequence: str
+    structure: str
+
+    @staticmethod
+    def from_string(sequence: str, structure: str):
+        if len(sequence) != len(structure):
+            raise ValueError(
+                "Sequence and structure lengths differ, {} vs {}",
+                (len(sequence), len(structure)),
+            )
+        return DotBracket(sequence, structure)
+
+    def __post_init__(self):
+        self.pairs = []
+
+        opening = "([{<ABC"
+        closing = ")]}>abc"
+        begins = {bracket: list() for bracket in opening}
+        matches = {end: begin for begin, end in zip(opening, closing)}
+        pairs = []
+
+        for i in range(len(self.structure)):
+            c = self.structure[i]
+            if c in opening:
+                begins[c].append(i)
+            elif c in closing:
+                begin = matches[c]
+                pairs.append((begins[begin].pop(), i))
+
+    def __str__(self):
+        return f"{self.sequence}\n{self.structure}"
+
+    def to_bpseq(self) -> BpSeq:
+        entries = [Entry(i + 1, self.sequence[i], 0) for i in range(len(self.sequence))]
+        for i, j in self.pairs:
+            entries[i][2] = j + 1
+            entries[j][2] = i + 1
+        return BpSeq(entries)
