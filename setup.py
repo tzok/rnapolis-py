fe3907255b1e276212e220756e47db8142bae8b1
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2023-08-02 15:46:16 +0200

    chore: set version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.1.1",
+    version="0.1.2",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
