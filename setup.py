9a328b2f73d22187d0ab2df8ce5d312bf2233663
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2023-11-09 12:21:12 +0100

    release: 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.1.4",
+    version="0.1.5",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
