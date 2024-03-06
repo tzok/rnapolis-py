c974dd69da728750c41f6bb529b8dc8571fa037c
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-03-06 13:41:31 +0100

    fix: add threading.Lock to solve synchronization problems (#158)
    * fix: add threading.Lock to solve synchronization problems
    
    * chore: set version to 0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.3.3",
+    version="0.3.4",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
