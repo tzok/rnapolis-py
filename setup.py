0e66a4a54ef348c058240af65d8c0bc45d41f093
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-03-12 09:13:23 +0100

    feat: use rfam defaults for infernal (#161)
    * feat: use Rfam defaults
    
    * chore: set version to 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.3.4",
+    version="0.3.5",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
