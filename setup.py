cedd4dd7ec37fba285cef9a092a86b65ad3cce0f
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-05-09 11:08:28 +0200

    feat: optimize all dot bracket generation (#190)
    * feat: optimize all dot-bracket generation
    
    * chore: set version to 0.3.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.3.14",
+    version="0.3.15",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
