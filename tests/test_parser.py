8bb3fca578a4cc9c97ecd04f20fe9cd039b0d85c
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-06-13 13:20:30 +0200

    fix: adjusted the start index for the "Residue name" entries (PDB) (#206)
    * fix: adjusted the start index for the "Residue name" entries (PDB)
    
    * feat: add test case to cover the bug
    
    ---------
    
    Co-authored-by: Diego Barquero Morera <diego.barquero-morera@phys.ens.fr>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,3 +9,10 @@ def test_nucleic_acid_only():
     with open("tests/184D.cif") as f:
         structure3d = read_3d_structure(f, 1, nucleic_acid_only=True)
     assert len(structure3d.residues) == 14
+
+
+def test_1ato():
+    with open("tests/1ATO.pdb") as f:
+        structure3d = read_3d_structure(f)
+    sequence = "".join([residue.one_letter_name for residue in structure3d.residues])
+    assert sequence == "GGCACCUCCUCGCGGUGCC"
