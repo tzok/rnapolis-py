20fe06c636f81908b41ffd392fc118ed4ebbf24c
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-07-01 15:36:59 +0200

    feat: support CSV output (#212)
    * feat: add support for CSV output
    
    * chore: set version to 0.3.17
    
    * build: bump orjson from 3.10.3 to 3.10.5 (#208)
    
    Bumps [orjson](https://github.com/ijl/orjson) from 3.10.3 to 3.10.5.
    - [Release notes](https://github.com/ijl/orjson/releases)
    - [Changelog](https://github.com/ijl/orjson/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/ijl/orjson/compare/3.10.3...3.10.5)
    
    ---
    updated-dependencies:
    - dependency-name: orjson
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build: bump scipy from 1.13.1 to 1.14.0 (#210)
    
    Bumps [scipy](https://github.com/scipy/scipy) from 1.13.1 to 1.14.0.
    - [Release notes](https://github.com/scipy/scipy/releases)
    - [Commits](https://github.com/scipy/scipy/compare/v1.13.1...v1.14.0)
    
    ---
    updated-dependencies:
    - dependency-name: scipy
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build: bump hypothesis from 6.103.1 to 6.104.2 (#211)
    
    Bumps [hypothesis](https://github.com/HypothesisWorks/hypothesis) from 6.103.1 to 6.104.2.
    - [Release notes](https://github.com/HypothesisWorks/hypothesis/releases)
    - [Commits](https://github.com/HypothesisWorks/hypothesis/compare/hypothesis-python-6.103.1...hypothesis-python-6.104.2)
    
    ---
    updated-dependencies:
    - dependency-name: hypothesis
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.3.16",
+    version="0.3.17",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
