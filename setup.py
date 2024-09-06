701b64b34c901dd419d6e5c4a87430b0fb3a01b2
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-09-06 10:16:36 +0200

    fix: resolve conflicts in BPSEQ generation (#236)
    * feat: add test case
    
    * fix: remove conflicts when generating BPSEQ
    
    * chore: set version to 0.4.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.4.3",
+    version="0.4.4",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
