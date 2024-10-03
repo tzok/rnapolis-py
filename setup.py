edfca23f3fbffe20208d10c9a49eb28084c24d4d
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-10-03 12:01:57 +0200

    feat: support alternate locations (#243)
    * feat: add test case with file containing altlocs
    
    * feat: make the test case more demanding
    
    * feat: add robust support for alternate locations of atoms
    
    * chore: set version to 0.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.4.4",
+    version="0.4.5",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -16,3 +16,18 @@ def test_1ato():
         structure3d = read_3d_structure(f)
     sequence = "".join([residue.one_letter_name for residue in structure3d.residues])
     assert sequence == "GGCACCUCCUCGCGGUGCC"
+
+
+def test_4qln_no_duplicate_atoms():
+    for ext in (".pdb", ".cif"):
+        with open(f"tests/4qln{ext}") as f:
+            structure3d = read_3d_structure(f)
+
+        chain_a = [r for r in structure3d.residues if r.auth.chain == "A"]
+        residues_to_check = [r for r in chain_a if r.auth.number in (18, 19, 20)]
+
+        for residue in residues_to_check:
+            atom_names = [atom.name for atom in residue.atoms]
+            assert len(atom_names) == len(
+                set(atom_names)
+            ), f"Duplicate atoms found in residue {residue.auth}"
