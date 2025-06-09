1d51f9158c074dd8c1f3768c6fa2ab5c39905300
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2025-06-09 16:39:15 +0200

    fix: adapt annotator to upstream changes (#358)
    * feat: update annotator to handle `all_dot_brackets` flag like adapter
    
    * chore: bump version to 0.9.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.9.0",
+    version="0.9.1",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
