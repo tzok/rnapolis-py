25b5c91d7fda79201d646b6e392c3b021ad29ed2
Tomasz Zok <tomasz.zok@gmail.com>
2025-10-22 09:10:34 +0200

    build: Bump version to 0.10.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.10.3",
+    version="0.10.4",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
