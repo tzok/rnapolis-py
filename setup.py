8cf38ecd1c793189411d950ef05d4a048f91de50
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2025-10-22 11:33:33 +0200

    fix: Return Residue object from match_dssr_name_to_residue (#428)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.10.7",
+    version="0.10.8",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
