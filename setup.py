0c47d74dcf9e190ba75d799a16459290b30b9cbe
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-11-04 17:54:01 +0100

    chore: bump version to 0.11.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="RNApolis",
-    version="0.11.1",
+    version="0.11.2",
     packages=["rnapolis"],
     package_dir={"": "src"},
     author="Tomasz Zok",
